b1a57f1ea0718ab3d43f16935c184bff59eeb960
Dumindu <dumindu.cse@gmail.com>
2024-04-11 19:52:04 +0530

    complete landing page

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/scripts");
     eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/videos");
+    eleventyConfig.addPassthroughCopy("src/404.html");
   
     return {
       dir: {
